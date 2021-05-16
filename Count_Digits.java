8bbf88987729a388f840a9f47532ceea5f007a70
Akshay Jain <jainakshay1783@gmail.com>
2021-05-17 03:55:42 +0530

    Count Digits : Iterative | Recursice | Logarithmic

---
+++ Count_Digits.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        long num = sc.nextLong();
+        System.out.println("Number of Digits (Iterative) : " + count_digit_itr(num));
+        System.out.println("Number of Digits (Recursive) : " + count_digit_recur(num));
+        System.out.println("Number of Digits (Logarithmic) : " + count_digit_log(num));
+    }
+
+    public static int count_digit_log(long num){
+        return (int)(Math.floor(1 + Math.log10(num)));
+    }
+
+    public static int count_digit_itr(long num){
+        int count = 0;
+        while(num!=0){
+            num = num / 10;
+            count++;
+        }
+        return count;
+    }
+
+    public static int count_digit_recur(long num){
+        if (num == 0)
+            return 0;
+
+        return 1 + count_digit_itr(num/10);
+    }
+}
